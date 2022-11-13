99be63584aa7e4afee3fa7adf3db415514eb5702
Shubh <Shubhamgaikwad496@gmail.com>
2022-11-13 22:49:16 +0530

    update

---
+++ src/main/java/com/Elemntry/DevlopApplication.java
@@ -0,0 +1,13 @@
+package com.Elemntry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlopApplication.class, args);
+	}
+
+}
